d6f11a70fd8e2a38118055da00ec74b7592ab418
Robert Ellis <robmadeyou@gmail.com>
2013-04-30 14:38:31 +0200

    Update Keyboard.java

--- src/com/gmail/robmadeyou/Input/Keyboard.java
+++ src/com/gmail/robmadeyou/Input/Keyboard.java
@@ -1,5 +1,30 @@
 package com.gmail.robmadeyou.Input;
 
 public class Keyboard {
-	//TODO DO KEYBOARD STUFF PLS
+	
+	public static boolean isKeyPressed(Key key){
+		if(key.isPressed){
+			return true;
+		}
+		return false;
+	}
+	public static boolean isKeyReleased(Key key){
+		if(key.isReleased){
+			return true;
+		}
+		return false;
+	}
+	public static boolean isKeyDown(Key key){
+		if(key.isDown){
+			return false;
+		}
+		return true;
+	}
+	public void onUpdate(){
+		while(Keyboard.next(){
+			if(Keyboard.getEventStateKey()){
+				if(Keyboad.isKeyDown(Keyboard.)
+			}
+		}
+	}
 }
