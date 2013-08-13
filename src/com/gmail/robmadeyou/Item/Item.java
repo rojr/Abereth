520bd469a190ea0290b73590e836b93cd72f3854
robmadeyou <robmadeyou@gmail.com>
2013-08-13 19:27:03 +0100

    Updated items

--- src/com/gmail/robmadeyou/Item/Item.java
+++ src/com/gmail/robmadeyou/Item/Item.java
@@ -1,9 +1,11 @@
 package com.gmail.robmadeyou.Item;
 
+import com.gmail.robmadeyou.Screen;
 import com.gmail.robmadeyou.Draw.Collector;
 import com.gmail.robmadeyou.Draw.Collector.DrawParameters;
 import com.gmail.robmadeyou.Effects.Color;
 import com.gmail.robmadeyou.Entity.Entity;
+import com.gmail.robmadeyou.Gui.Gui;
 import com.gmail.robmadeyou.Input.Mouse;
 import com.gmail.robmadeyou.World.World;
 
@@ -13,6 +15,17 @@ public class Item {
 	private int id;
 	private int Texture;
 	private int layer;
+	private boolean wasPressed = false;
+	private boolean isInGui = false;
+	private Gui inGui = null;
+	
+	
+	//Possibly making some gravity effect
+	//when the items are let go, they slow down
+	//instead of just falling straight down
+	
+	//private int lastDX = 0, lastDY = 0;
+	//private double leftDX = 0, leftDY = 0;
 	
 	public Item(double x, double y, int width, int height, int id, int Texture){
 		this.setX(x);
@@ -102,24 +115,44 @@ public class Item {
 		}
 		return false;
 	}
-	
+	/*
+	 * Boolean checking if the item is being held, only returned true if the mouse is
+	 * inside the item, this can be tricky to work with as the mouse would move too 
+	 * fast from the item and the mouse would end up not being inside the bounds of the item
+	 * use isDragged better;
+	 */
 	public boolean isHeld(){
 		if(Mouse.leftMouseButtonDown && isMouseOver()){
+			wasPressed = true;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isDragged(){
+		isHeld();
+		if(!Mouse.leftMouseButtonDown && wasPressed){
+			wasPressed = false;
+			System.out.println(Mouse.getDX());
+			return false;
+		}
+		if(wasPressed){
 			return true;
 		}
 		return false;
 	}
 	
 	public void onUpdate(){
-		if(isHeld()){
+		if(isDragged()){
 			x += Mouse.getDX();
 			y += Mouse.getDY();
+		}else{
+			if(!World.isSolidAtLocation((int)Math.round(x / World.BLOCK_SIZE()), (int)Math.round(y / World.BLOCK_SIZE()))){
+				y += World.gravity(Screen.delta) * 10;
+			}
 		}
-		//TODO work in progress
-		//if(!World.isSolidAtLocation(x / World.BLOCK_SIZE(), y / World.BLOCK_SIZE()){
-		//	
-		//}
 	}
+	
 	public void draw(){
 		Collector.add(new DrawParameters("box", x, y, width, height, Texture, Color.White, 1, layer, true));
 	}
