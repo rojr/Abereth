4cf6e81cb0a9de08967474cb3566845e4892210b
Robert Ellis <robmadeyou@gmail.com>
2013-09-25 09:46:26 +0100

    Update Line.java

--- src/com/gmail/robmadeyou/Draw/Line.java
+++ src/com/gmail/robmadeyou/Draw/Line.java
@@ -3,6 +3,12 @@ package com.gmail.robmadeyou.Draw;
 import static org.lwjgl.opengl.GL11.*;
 
 public class Line {
+    /**
+     * Simply draws lines, this is used by the rendering system to draw single lines.
+     * 
+     * !!It is advised against to use this as well as the rendering system as independant calls would
+     * most likely not draw lines (unless called after the Screen.refresh() method!!
+     */
     public static void drawLine(float x, float y, float x2, float y2) {
         glBegin(GL_LINES);
         glVertex2f(x, y);
