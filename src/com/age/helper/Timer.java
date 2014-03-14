e401e93056950a1221c9e3a20b840a9dcb404494
Robmadeyou <robmadeyou@gmail.com>
2014-03-14 16:34:45 +0000

    #20 Beginning to re-write tests

---
+++ src/com/age/helper/Timer.java
@@ -0,0 +1,25 @@
+package com.age.helper;
+
+import java.util.ArrayList;
+
+/**
+ * Created by apex on 14/03/14.
+ */
+public class Timer {
+    ArrayList<Timer>  timers = new ArrayList<Timer>();
+    long duration;
+    long now = System.nanoTime();
+
+    /**
+     *
+     * @param duration Duration is in Nanoseconds
+     */
+    public Timer(long duration){
+
+    }
+
+    public void done(){
+
+    }
+
+}
--- src/com/age/tests/BubbleSort.java
+++ src/com/age/tests/BubbleSort.java
@@ -1,35 +1,44 @@
 package com.age.tests;
 
+import com.age.Game;
 import com.age.Screen;
+import com.age.View;
 import com.age.graphics.render.shapes.Box;
 
 import java.util.Random;
 
-public class BubbleSort {
+public class BubbleSort extends View{
 
     static Box[] toSort = new Box[100];
     static int index = 0;
     static int times = 0;
     public BubbleSort(){
-        Screen.create(600,600, "BubbleSort");
+        super("BubbleSort");
+    }
+
+    @Override
+    public void update() {
+        toSort = bubbleSort(toSort);
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void init() {
         Random ran = new Random();
         for(int i = 0; i < toSort.length; i++){
             int rando = ran.nextInt(Screen.getHeight());
             System.out.println(rando);
             toSort[i] = (Box) new Box(6 * i, 0, 6, rando).toEngine();
         }
-
-        while(!Screen.isCloseRequested()){
-            Screen.update();
-
-            toSort = bubbleSort(toSort);
-
-            Screen.refresh(60);
-        }
     }
 
     public static void main(String... args){
-        new BubbleSort();
+        Game g = new Game("BubbleSort", 600,400);
+        g.start(new BubbleSort());
     }
 
     public static Box[] bubbleSort(Box[] toSort){
