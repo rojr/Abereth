2fa2e7f4580784abf842b66fef9f8c10e797ad77
Robert Ellis <robmadeyou@gmail.com>
2014-11-17 13:19:48 +0000

    Fixed really stupid naming change

---
+++ src/com/abereth/tests/events/EventTest.java
@@ -0,0 +1,52 @@
+package com.abereth.tests.events;
+
+import com.abereth.G;
+import com.abereth.event.ViewEvent;
+import com.abereth.game.Game;
+import com.abereth.game.View;
+import com.abereth.input.Mouse;
+import com.abereth.tests.trianges.Point;
+
+/**
+ * Created by jeremiah on 12/11/2014.
+ */
+public class EventTest extends View
+{
+	public static void main ( String[] args )
+	{
+		Game g = new Game( 1024, 512 );
+		g.addView( new EventTest( g ) );
+		g.start();
+	}
+
+	public EventTest ( Game game )
+	{
+		super( game );
+		getEventManager().add( new ViewEvent( )
+		{
+			@Override
+			public boolean isDone ( View view )
+			{
+				return Mouse.isLeftMouseClicked( );
+			}
+
+			@Override
+			public void OnUpdate ( int delta, View view )
+			{
+				if( Math.random() >= 0.9 )
+				{
+					view.add( new Point( (int) (Math.random() * G.WIDTH), (int)(Math.random() * G.HEIGHT ) ) );
+				}
+			}
+		}, false );
+	}
+
+	@Override
+	public void update ( int delta )
+	{
+		if( Mouse.isLeftMouseClicked() )
+		{
+			System.out.println( "Happy chimney!" );
+		}
+	}
+}
